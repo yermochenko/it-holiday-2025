fac477535a8d8d678c62c66215c416a16362d65f
Sergey Yermochenko <yermochenko@gmail.com>
2025-06-19 13:57:41 +0300

    Консольное приложение

--- console/src/main/java/Runner.java
+++ console/src/main/java/Runner.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
+
+import static java.lang.Math.toRadians;
+
 public class Runner {
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
+		System.out.println("Бросок тела под углом к горизонту");
+		Scanner in = new Scanner(System.in);
+		Body body = new Body();
+		System.out.print("Угол (по умолчанию 30°): ");
+		try {
+			body.setAngel(toRadians(Double.parseDouble(in.nextLine())));
+		} catch(NumberFormatException e) {
+			body.setAngel(toRadians(30));
+		}
+		System.out.print("Масса (по умолчанию 1 кг): ");
+		try {
+			body.setMass(Double.parseDouble(in.nextLine()));
+		} catch(NumberFormatException e) {
+			body.setMass(1);
+		}
+		System.out.print("Коэффициент сопротивления (по умолчанию 0.3 кг/с): ");
+		try {
+			body.setResistanceCoefficient(Double.parseDouble(in.nextLine()));
+		} catch(NumberFormatException e) {
+			body.setResistanceCoefficient(0.3);
+		}
+		System.out.print("Начальная скорость (по умолчанию 20 м/с): ");
+		try {
+			body.setStartSpeed(Double.parseDouble(in.nextLine()));
+		} catch(NumberFormatException e) {
+			body.setStartSpeed(20);
+		}
+		double length = Model.flyingDistance(body);
+		System.out.printf("Дальность полёта %.2f метров\n", length);
 	}
 }
